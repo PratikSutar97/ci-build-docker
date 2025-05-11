20bed19d9d095001a53e2083caa0885948e6514a
cloudmlops <84102103+cloudmlops@users.noreply.github.com>
2025-05-11 13:19:50 +0530

    Update app.py

--- docker_streamlit_dashboard/src/app.py
+++ docker_streamlit_dashboard/src/app.py
@@ -12,7 +12,8 @@ This very simple webapp allows you to select and visualize players from certain 
 # S3_BUCKET_NAME=os.environ['S3_BUCKET_NAME']
 # CSV_FILE_NAME=os.environ['CSV_FILE_NAME']
 # S3_FILE_PATH='s3://' + str(S3_BUCKET_NAME) + '/' + str(CSV_FILE_NAME)
-df = st.cache(pd.read_csv)("football_data.csv")
+# df = st.cache(pd.read_csv)("football_data.csv")
+df = st.cache_data(lambda: pd.read_csv("football_data.csv"))()
 # df = pd.read_csv("s3://my-test-bucket/sample.csv")
 # df = pd.read_csv(S3_FILE_PATH)
 
