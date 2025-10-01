f75bb27e32fc6ac9b4961b406c106d1176301d73
Pratik Sutar <pratiksutar1901@gmail.com>
2025-10-01 13:13:32 +0530

    Update app.py

--- docker_python/src/app.py
+++ docker_python/src/app.py
@@ -5,10 +5,10 @@ import numpy as np
 
 st.set_page_config(page_title="Financial Planning Calculator")
 
-st.title("Financial Planning Calculator-May-2025")
+st.title("Financial Planning Calculator-March-2025")
 
-st.header("**Monthly Income-May-2025**")
-st.subheader("Salary-May-2025")
+st.header("**Monthly Income-March-2025**")
+st.subheader("Salary-March-2025")
 colAnnualSal, colTax = st.columns(2)
 
 with colAnnualSal:
